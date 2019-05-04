70c25c80f6ca01070d62ba821173c4ea2663d7ca
leixinzhou <llasionzhou@gmail.com>
2019-05-04 16:23:15 -0500

    0.5 dataset

--- MoNuDataset.py
+++ MoNuDataset.py
@@ -8,13 +8,14 @@ import numpy as np
 import torch
 import os, random
 import matplotlib.pyplot as plt
+import cv2
 
 
 
 
-ROW_LEN = 128
+ROW_LEN = 96
 COL_LEN = 256
-SIGMA = 15.
+SIGMA = 10.
 
 
 def gaus_pdf(x_arry, mean, sigma, A=1.):
@@ -38,7 +39,7 @@ for i in range(ROW_LEN):
 class IVUSDataset(Dataset):
     """convert 3d dataset to Dataset."""
 
-    def __init__(self, case_list, gaus_gt=True, transform=None, batch_nb=None):
+    def __init__(self, img_np, gt_np, gaus_gt=True, transform=None, batch_nb=None):
         """
         Args:
             case_list (of dictionary): all  cases
@@ -46,28 +47,33 @@ class IVUSDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
             batch_nb (int, optional): can use to debug.
         """
-        self.case_list = case_list
+        self.img_np = np.load(img_np, mmap_mode='r')
+        self.gt_np = np.load(gt_np, mmap_mode='r')
         self.bn = batch_nb
         self.gaus_gt = gaus_gt
         self.transform = transform
 
     def __len__(self):
         if self.bn is None:
-            return len(self.case_list)
+            return self.img_np.shape[0]
         else:
             return self.bn
 
     def __getitem__(self, idx):
-        img_dir = self.case_list[idx]['img_dir']
-        gt_dir = self.case_list[idx]['gt_dir']
-        img = plt.imread(img_dir)
-        gt = np.loadtxt(gt_dir, delimiter=',')
+        img = self.img_np[idx,]
+        gt = self.gt_np[idx,]
+        # convert region gt to surface gt
+        ret,thresh = cv2.threshold(imgray,127,255,0)
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        gt_surf = contours[0]
+
         # convert to polar
-        phy_radius = 0.5*np.sqrt(np.average(np.array(img.shape)**2)) - 1
-        cartpolar = CartPolar(np.array(img.shape)/2.,
+        img_shape = img.shape[:-1]
+        phy_radius = 0.5*np.sqrt(np.average(np.array(img_shape)**2)) - 1
+        cartpolar = CartPolar(np.array(img_shape)/2.,
                               phy_radius, COL_LEN, ROW_LEN)
         polar_img = cartpolar.img2polar(img)
-        polar_gt = cartpolar.gt2polar(gt)
+        polar_gt = cartpolar.gt2polar(gt_surf)
         input_img_gt = {'img': polar_img, 'gt': polar_gt}
 
         # apply augmentation transform
@@ -76,15 +82,12 @@ class IVUSDataset(Dataset):
 
         # print(input_img_gt['gt'].shape)
         if self.gaus_gt:
-            polar_gt_gaus = np.empty_like(polar_img)
+            polar_gt_gaus = np.empty_like(polar_img[:,:,0])
             for i in range(COL_LEN):
                 polar_gt_gaus[:, i] = LU_TABLE[int(
                     np.clip(np.around(input_img_gt['gt'][i]), 0, ROW_LEN-1)), ]
             input_img_gt['gaus_gt'] = polar_gt_gaus
         input_img_gt['img'] = np.expand_dims(input_img_gt['img'], axis=0)
         input_img_gt = {key:value.astype(np.float32) for (key, value) in input_img_gt.items()}
-        # add gt_dir to the output
-        input_img_gt['gt_dir'] = gt_dir
-        input_img_gt['img_dir'] = img_dir
     
         return input_img_gt
